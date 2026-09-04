63567394e52ce1b7644ffead8cce9942083df4cb
Obito-root <shilpakuwarchouhan@gmail.com>
2026-09-04 17:00:55 +0530

    Create __init__.py

---
+++ src/payloads/__init__.py
@@ -0,0 +1,11 @@
+"""
+PayForge Payloads Module
+Contains all exploitation and testing modules
+"""
+
+__all__ = [
+    'nmap_scanner',
+    'web_vulnerability_scanner',
+    'payload_generator',
+    'subdomain_enumeration'
+]
